bd596f7819d964be179ae01e3c9757d0c34d5029
Shah Faisal <faisalgamerz098@gmail.com>
2026-08-19 13:07:10 +0530

    Add random password generator
    Implement a random password generator that prompts for password length and generates a password using letters, digits, and special characters.

---
+++ FaisalCodes/random_pass.py
@@ -0,0 +1,19 @@
+"""
+Code Name : Random Password Generator
+Author : Shah Faisal
+GitHub : @faisalg1t
+"""
+
+import random
+import string
+
+length = int(input("Password length: "))
+
+characters = string.ascii_letters + string.digits + "!@#$%^&*"
+
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+print("🔐 Your password:", password)
